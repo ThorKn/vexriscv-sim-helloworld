aa5d364d4acb7e94262cb7e738d9326c450aa94b
Thorsten Knoll <thorsten_knoll@yahoo.de>
2021-09-27 14:35:38 +0200

    update project

--- vexriscv/src/main/scala/mupq/PQVexRiscvSim.scala
+++ vexriscv/src/main/scala/mupq/PQVexRiscvSim.scala
@@ -2,6 +2,8 @@ package mupq
 
 import java.io.{File, FileInputStream, FileOutputStream, IOException, OutputStream}
 
+import com.sun.jna.Native;
+
 import scopt.OptionParser
 
 import spinal.sim._
@@ -18,28 +20,32 @@ import spinal.lib.com.jtag.sim.JtagTcp
 
 import vexriscv.VexRiscv
 import vexriscv.plugin.Plugin
+import java.sql.PseudoColumnUsage
+import java.util.Arrays
+import scala.collection.mutable.ArrayBuffer
 
-case class PipelinedMemoryBusRam(size : BigInt, initialContent : File = null) extends Component{
+case class PipelinedMemoryBusRam(size: BigInt, initialContent: File = null) extends Component {
   require(size % 4 == 0, "Size must be multiple of 4 bytes")
   require(size > 0, "Size must be greater than zero")
   val busConfig = PipelinedMemoryBusConfig(log2Up(size), 32)
-  val io = new Bundle{
+  val io = new Bundle {
     val bus = slave(PipelinedMemoryBus(busConfig))
   }
 
   val ram = Mem(Bits(32 bits), size / 4)
-  io.bus.rsp.valid := RegNext(io.bus.cmd.fire && !io.bus.cmd.write) init(False)
-  io.bus.rsp.data := ram.readWriteSync(
+  io.bus.rsp.valid := Delay(io.bus.cmd.fire && !io.bus.cmd.write, 2, init = False)
+  val rdata = ram.readWriteSync(
     address = io.bus.cmd.address >> 2,
-    data  = io.bus.cmd.data,
-    enable  = io.bus.cmd.valid,
-    write  = io.bus.cmd.write,
-    mask  = io.bus.cmd.mask
+    data = io.bus.cmd.data,
+    enable = io.bus.cmd.valid,
+    write = io.bus.cmd.write,
+    mask = io.bus.cmd.mask
   )
+  io.bus.rsp.data := RegNext(rdata) init (0)
   io.bus.cmd.ready := True
 
   if (initialContent != null) {
-    val input = new FileInputStream(initialContent)
+    val input       = new FileInputStream(initialContent)
     val initContent = Array.fill[BigInt](ram.wordCount)(0)
     val fileContent = Array.ofDim[Byte](Seq(input.available, initContent.length * 4).min)
     input.read(fileContent)
@@ -51,23 +57,21 @@ case class PipelinedMemoryBusRam(size : BigInt, initialContent : File = null) ex
   }
 }
 
-
-
-
 class PQVexRiscvSim(
-  val ramBlockSizes : Seq[BigInt] = Seq[BigInt](256 KiB, 128 KiB),
-  val initialContent : File = null,
-  val coreFrequency : HertzNumber = 12 MHz,
-  cpuPlugins : () => Seq[Plugin[VexRiscv]] = PQVexRiscv.withDSPMultiplier()
-) extends PQVexRiscv(
+  val ramBlockSizes: Seq[BigInt] = Seq[BigInt](256 KiB, 128 KiB),
+  val initialContent: File = null,
+  val coreFrequency: HertzNumber = 12 MHz,
+  cpuPlugins: () => Seq[Plugin[VexRiscv]] = PQVexRiscv.withDSPMultiplier()
+)
+extends PQVexRiscv(
   cpuPlugins = cpuPlugins,
-  ibusRange = SizeMapping(0x80000000l, ramBlockSizes.reduce(_ + _))
+  ibusRange = SizeMapping(0x80000000L, ramBlockSizes.reduce(_ + _))
 ) {
   val io = new Bundle {
-    val asyncReset = in Bool
-    val mainClock = in Bool
-    val uart = master(Uart())
-    val jtag = slave(Jtag())
+    val asyncReset = in Bool()
+    val mainClock  = in Bool()
+    val uart       = master(Uart())
+    val jtag       = slave(Jtag())
   }
 
   asyncReset := io.asyncReset
@@ -77,8 +81,10 @@ class PQVexRiscvSim(
   jtag <> io.jtag
 
   val memory = new ClockingArea(systemClockDomain) {
-    val ramBlocks = ramBlockSizes.zipWithIndex.map(t => PipelinedMemoryBusRam(t._1, if (t._2 == 0) initialContent else null))
-    var curAddr : BigInt = 0x80000000l
+    val ramBlocks =
+      ramBlockSizes.zipWithIndex.map(t =>
+        PipelinedMemoryBusRam(t._1, if (t._2 == 0) initialContent else null))
+    var curAddr: BigInt = 0x80000000L
     for (block <- ramBlocks) {
       busSlaves += block.io.bus -> SizeMapping(curAddr, block.size)
       curAddr += block.size
@@ -91,31 +97,50 @@ object PQVexRiscvSim {
   def main(args: Array[String]) = {
     case class PQVexRiscvSimConfig(
       uartOutFile: OutputStream = System.out,
+      enablePts: Boolean = false,
       initFile: File = null,
       ramBlocks: Seq[BigInt] = Seq(256 KiB, 128 KiB),
       cpuPlugins: () => Seq[Plugin[VexRiscv]] = PQVexRiscv.withDSPMultiplier()
     )
     val optParser = new OptionParser[PQVexRiscvSimConfig]("PQVexRiscvSim") {
       head("PQVexRiscvSim simulator")
-      help("help") text("print usage text")
-      opt[File]("uart") action((f, c) => c.copy(uartOutFile = new FileOutputStream(f, true))) text("File for UART output (will be appended)") valueName("<output>")
-      opt[File]("init") action((f, c) => c.copy(initFile = f)) text("Initialization file for first RAM block") valueName("<bin>")
-      opt[Seq[Int]]("ram") action((r, c) => c.copy(ramBlocks = r.map(_ KiB))) text("SRAM Blocks in KiB") valueName("<block1>,<block2>")
+      help("help") text ("print usage text")
+      opt[File]("uart") action ((f, c) =>
+        c.copy(uartOutFile =
+          new FileOutputStream(
+            f,
+            true))) text ("File for UART output (will be appended)") valueName ("<output>")
+      opt[Unit]("pts") action ((_, c) => c.copy(enablePts = true))
+      opt[File]("init") action ((f, c) =>
+        c.copy(initFile = f)) text ("Initialization file for first RAM block") valueName ("<bin>")
+      opt[Seq[Int]]("ram") action ((r, c) =>
+        c.copy(ramBlocks =
+          r.map(_ KiB))) text ("SRAM Blocks in KiB") valueName ("<block1>,<block2>")
     }
 
     val config = optParser.parse(args, PQVexRiscvSimConfig()) match {
       case Some(config) => config
-      case None => ???
+      case None         => ???
     }
 
     val compiled = SimConfig.allOptimisation.compile {
-      new PQVexRiscvSim(config.ramBlocks, config.initFile, cpuPlugins=config.cpuPlugins)
+      new PQVexRiscvSim(
+        config.ramBlocks,
+        config.initFile,
+        cpuPlugins = config.cpuPlugins
+      )
+    }
+
+    var pts = if (config.enablePts) new PseudoTerminal() else null
+
+    if (pts != null) {
+      println(f"Opened pts under ${pts.getPtsName}, UART output will be directed there.")
     }
 
     compiled.doSim("PqVexRiscvSim", 42) { dut =>
-      val mainClkPeriod = (1e12 / dut.coreFrequency.toDouble).toLong
-      val jtagClkPeriod = mainClkPeriod * 4
-      val uartBaudRate = 115200
+      val mainClkPeriod  = (1e12 / dut.coreFrequency.toDouble).toLong
+      val jtagClkPeriod  = mainClkPeriod * 4
+      val uartBaudRate   = 115200
       val uartBaudPeriod = (1e12 / uartBaudRate.toDouble).toLong
 
       val clockDomain = ClockDomain(dut.io.mainClock, dut.io.asyncReset)
@@ -128,30 +153,35 @@ object PQVexRiscvSim {
 
       println(s"Simulating ${dut.getClass.getName} with JtagTcp on port 7894")
 
-      val uartPin = dut.io.uart.txd
+      val uartTxd = dut.io.uart.txd
+      val uartRxd = dut.io.uart.rxd
 
       val uartDecoder = fork {
         sleep(1)
-        waitUntil(uartPin.toBoolean == true)
+        waitUntil(uartTxd.toBoolean == true)
         try {
           while (true) {
-            waitUntil(uartPin.toBoolean == false)
+            waitUntil(uartTxd.toBoolean == false)
             sleep(uartBaudPeriod / 2)
-            if (uartPin.toBoolean != false) {
+            if (uartTxd.toBoolean != false) {
               println("\rUART frame error (start bit)")
             } else {
               sleep(uartBaudPeriod)
               var byte = 0
-              var i = 0
+              var i    = 0
               while (i < 8) {
-                if (uartPin.toBoolean) {
+                if (uartTxd.toBoolean) {
                   byte |= 1 << i
                 }
                 sleep(uartBaudPeriod)
                 i += 1
               }
-              if (uartPin.toBoolean) {
-                config.uartOutFile.write(byte)
+              if (uartTxd.toBoolean) {
+                if (pts != null) {
+                  pts.writeByte(byte)
+                } else {
+                  config.uartOutFile.write(byte)
+                }
               } else {
                 println("\rUART frame error (stop bit)")
               }
@@ -161,6 +191,27 @@ object PQVexRiscvSim {
           case io: IOException =>
         }
         println("\rUART decoder stopped")
+      }
+
+      val uartEncoder = if (pts != null) fork {
+        uartRxd #= true
+        while (true) {
+          val b = pts.readByte()
+          if (b != -1) {
+            uartRxd #= false
+            sleep(uartBaudPeriod)
+
+            (0 to 7).foreach { bitId =>
+              uartRxd #= ((b >> bitId) & 1) != 0
+              sleep(uartBaudPeriod)
+            }
+
+            uartRxd #= true
+            sleep(uartBaudPeriod)
+          } else {
+            sleep(uartBaudPeriod * 1000)
+          }
+        }
       }
 
       var running = true
